2e62e7771bfa32a722d9fbbd8f956b45c3abf507
Arturo Salvio <775410+asalvi0@users.noreply.github.com>
2024-01-04 19:42:33 -0600

    added util methods

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,1 +1,29 @@
 package utils
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/goccy/go-json"
+	"github.com/zeebo/xxh3"
+)
+
+func ValidateInput[T any](item T) error {
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	if err := validate.Struct(item); err != nil {
+		return err
+	}
+	return nil
+}
+
+func GenerateMessageID[T any](data T) string {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return ""
+	}
+
+	hash := xxh3.Hash(jsonData)
+	id := fmt.Sprintf("%x", hash)
+
+	return id
+}
