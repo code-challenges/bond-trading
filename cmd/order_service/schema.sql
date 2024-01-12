02cefbf22f7e43bc3d9facd558e68dbc6117332c
Arturo Salvio <775410+asalvi0@users.noreply.github.com>
2024-01-11 23:18:58 -0600

    updated b schema

--- cmd/order_service/schema.sql
+++ cmd/order_service/schema.sql
@@ -10,30 +10,32 @@ alter type orderstatus owner to postgres;
 
 create table if not exists users
 (
-    id            bigint                   not null constraint users_pk primary key,
-    username      varchar                  not null,
-    email         varchar                  not null,
-    password_hash varchar                  not null,
-    created_at    timestamp with time zone not null,
-    updated_at    timestamp with time zone,
-    active        boolean                  not null
+    id            bigint            not null constraint users_pk primary key,
+    username      varchar           not null,
+    email         varchar           not null,
+    password_hash varchar           not null,
+    created_at    timestamptz       not null,
+    updated_at    timestamptz,
+    active        boolean           not null
 );
-
 alter table users owner to postgres;
 
 create table if not exists orders
 (
-    id          varchar                  not null constraint orders_pk primary key,
-    user_id     bigint                   not null constraint orders_users_fk references users,
-    bond_id     varchar                  not null,
-    quantity    integer                  not null,
-    filled      money                    not null,
-    price       money                    not null,
-    action      orderaction              not null,
-    status      orderstatus              not null,
-    expiress_at timestamp with time zone not null,
-    created_at  timestamp with time zone not null,
-    updated_at  timestamp with time zone
+    id          varchar             not null constraint orders_pk primary key,
+    user_id     bigint              not null constraint orders_users_fk references users,
+    bond_id     integer             not null,
+    quantity    integer             not null,
+    filled      integer             not null,
+    price       decimal(9, 4)       not null,
+    action      orderaction         not null,
+    status      orderstatus         not null,
+    expires_at  timestamptz         not null,
+    created_at  timestamptz         not null,
+    updated_at  timestamptz
 );
-
 alter table orders owner to postgres;
+
+-- create dev user/123
+insert into users (id, username, email, password_hash, created_at, active)
+    values (1, 'dev', 'dev@localhost', '$2a$10$bmPeLZ/DPG.7f7PbcoC.g.F0jZ4KPl64Tr4p8kxNJ/jRz0LFwPe9K', now(), true);
